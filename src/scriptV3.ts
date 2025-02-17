05a3aafaa55bf900bcafe544a90b508d78313de9
genna-cervantes <gennacervantes9@gmail.com>
2025-02-17 22:18:47 +0800

    feat: get curriculum per year department specialization

--- src/scriptV3.ts
+++ src/scriptV3.ts
@@ -2,6 +2,8 @@
 //   - eto ung iccross over multiple times para marami like possible something
 //   - tapos saka mag aassign sa baba
 
+import { Client } from "pg";
+
 // assign rooms while checking conflict
 //   - add sa room sched tapos don mag check ng conflict
 // assign tas while checking conflict
@@ -23,7 +25,31 @@
 //     csc: 'datasci'
 // }
 
-const generateV3 = ({
+const DB_HOST = 'localhost';
+const DB_PORT = 5432;
+const DB_USER = 'postgres';
+const DB_PASSWORD = 'password';
+const DB_NAME = 'postgres';
+
+export const client = new Client({
+    host: DB_HOST,
+    port: DB_PORT, // Use a default port if not specified
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME
+});
+
+// Connect to PostgreSQL
+client
+    .connect()
+    .then(() => {
+        console.log('Connected to PostgreSQL database');
+    })
+    .catch((err) => {
+        console.error('Connection error', err.stack);
+    });
+
+const generateV3 = async ({
     department,
     year,
     sectionSpecializations
@@ -32,11 +58,36 @@ const generateV3 = ({
     year: number;
     sectionSpecializations: any;
 }) => {
+    let specializationsAndSections: any = {}
+    let specializationsAndCurriculum: any = {}
 
     // group sectionSpecializations by specialization not section
     let sectionKeys = Object.keys(sectionSpecializations);
-    // for (let i = 0; i < sectionKeys)
+    for (let i = 0; i < sectionKeys.length; i++){
+        if (!specializationsAndSections[sectionKeys[i]]){
+            specializationsAndSections[sectionKeys[i]] = []
+        }
+
+        specializationsAndSections[sectionKeys[i]].push(sectionKeys[i])
+    }
 
     // get curriculum per year per department per specialization
-    let specializations = Object.keys(sectionSpecializations)
+    let specializations = Object.keys(specializationsAndSections)
+
+    if (specializations.length > 0){
+        for (let i = 0; i < specializations.length; i++){
+            const query = 'SELECT courses FROM curriculum WHERE department = $1 AND year = $2 AND specialization = $3'
+            const res = await client.query(query, [department, year, specializations[i]])
+            const curriculum = res.rows[0].curriculum
+            
+            specializationsAndCurriculum[specializations[i]] = curriculum;
+        }
+    }else{
+        const query = 'SELECT courses FROM curriculum WHERE department = $1 AND year = $2'
+        const res = await client.query(query, [department, year])
+        const curriculum = res.rows[0].curriculum
+
+        specializationsAndCurriculum['all'] = curriculum;
+    }
+
 };
